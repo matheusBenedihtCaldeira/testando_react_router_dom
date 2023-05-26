58a22319fd7ea6570e27267b9fc83ba8ebc0e91c
bene <matheusbenediht@gmail.com>
2023-05-26 12:21:18 -0300

    Fazendo rotas entre componentes

---
+++ src/components/Redirect/index.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useRef, useState } from 'react';
+import './style.css'
+import { useNavigate } from 'react-router-dom';
+
+export const Redirect = () =>{
+    const [time, setTime] = useState(3);
+    const timeout = useRef(0);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        clearTimeout(timeout.current);
+        timeout.current = setTimeout(() => {
+            setTime((t) => t - 1)
+        }, 1000)
+    }, [time])
+
+    return(
+        <div>
+            <h1>Saindo daqui em: {time}</h1>
+        </div>
+    )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles/global.css'
 import { Home } from './components/Home'
+import { About } from './components/About'
+import { Post} from './components/Post'
+import { Menu } from './components/Menu'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { Redirect } from './components/Redirect'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Home />
+    <BrowserRouter>
+    <Menu />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path ='/about' element={<About/>} />
+        <Route path ='/posts/:id' element={<Post/>} />
+        <Route path ='/post' element={<Post/>} />
+        <Route path ='/redirect' element={<Redirect/>} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
